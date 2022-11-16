2da2a532331e1301c84aeaf8b213fbc97a2df7a4
fernanda5010 <118388317+fernanda5010@users.noreply.github.com>
2022-11-16 12:39:25 -0600

    primercommit

---
+++ aplicacion_01/lib/src/page/home_page.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+     appBar: AppBar(
+      title: Text('aplicacion clase'),
+     )
+     body: Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.center,
+     ) 
+
+      children: [
+        Text ('')
+        Text ('')
+      ],
+
+    );
+  }
+}
